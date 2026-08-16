ca7832b9429a92137125277369c4f4e22b1d0416
Andrejr82 <devmauriciojr@gmail.com>
2026-08-16 08:56:37 -0300

    fix(reels): trace sharp runtime dependencies

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
       './node_modules/@img/sharp-linux-x64/**/*',
       './node_modules/@img/sharp-libvips-linux-x64/**/*',
     ],
+    '/api/reels/scenes': [
+      './node_modules/sharp/**/*',
+      './node_modules/@img/sharp-linux-x64/**/*',
+      './node_modules/@img/sharp-libvips-linux-x64/**/*',
+    ],
   },
   images: {
     unoptimized: true,
